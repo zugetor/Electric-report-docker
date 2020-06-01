c83bec9cc6787a1cedae96b1d535ccb00bfe97ab
Narawit <dekter123456@gmail.com>
2020-06-01 19:01:22 +0700

    modify DB , dev API , route and tested

--- Docker/web/db/Query.py
+++ Docker/web/db/Query.py
@@ -12,7 +12,7 @@ class Query:
         self._client = client
         
     def demo1(self):
-        self._cur.execute('''SELECT * FROM user''')
+        self._cur.execute("SELECT * FROM user")
         res = self._cur.fetchall()
         return res
         
@@ -21,28 +21,24 @@ class Query:
         return result.raw
         
     def sensor_list(self):
-        self._cur.execute('''SELECT s.sid,s.sname,t.tname,r.rname,f.fname,b.bname,bo.bomac FROM sensor AS s INNER JOIN board AS bo ON (s.boid=bo.boid) INNER JOIN room AS r ON (bo.rid=r.rid) INNER JOIN floor AS f ON (r.fid=f.fid) INNER JOIN building AS b ON (f.bid=b.bid) INNER JOIN type as t ON (s.tid=t.tid)''')
+        self._cur.execute("SELECT s.sid,s.sname,t.tname,r.rname,f.fname,b.bname,bo.bomac FROM sensor AS s INNER JOIN board AS bo ON (s.boid=bo.boid) INNER JOIN room AS r ON (bo.rid=r.rid) INNER JOIN floor AS f ON (r.fid=f.fid) INNER JOIN building AS b ON (f.bid=b.bid) INNER JOIN type as t ON (s.tid=t.tid)")
         res = self._cur.fetchall()
         return res
         
-    def room_list(self):
-        self._cur.execute('''SELECT * FROM building''')
+    def all_room_list(self):
+        self._cur.execute("SELECT * FROM building")
         building = self._cur.fetchall()
-        self._cur.execute('''SELECT * FROM floor''')
-        floor = self._cur.fetchall()
-        self._cur.execute('''SELECT * FROM room''')
-        room = self._cur.fetchall()
         for b in building:
             b.update({"floor":[]})
-            
+            self._cur.execute("SELECT fid,fname FROM floor WHERE bid = %s",(b['bid']))
+            floor = self._cur.fetchall()
             for f in floor:
-                f.update({'room':[]});
-                if(b['bid']==f['bid']):  # select * from floor where f.bid= %s (b.bid)
-                    for r in room: 
-                        if(f['fid']==r['fid']): # select * from room where r.fid= %s (f.fid)
-                            f['room'].append({'rid':r['rid'],'rname':r['rname']})
-                    b['floor'].append({'fid':f['fid'],'fname':f['fname'],'room':f['room']})
-                    
+                f.update({'room':[]})
+                self._cur.execute("SELECT rid,rname FROM room WHERE fid = %s",(f['fid']))
+                room = self._cur.fetchall()
+                for r in room: 
+                    f['room'].append(r)
+                b['floor'].append(f)        
         return building
         
     def sensor_edit(self,sid,sname):
@@ -52,26 +48,74 @@ class Query:
         self._cur.execute("DELETE FROM sensor WHERE sid = %s",(sid))
         
     def register_list(self):
-        self._cur.execute('''SELECT * FROM board''')
+        self._cur.execute("SELECT * FROM board")
         board = self._cur.fetchall()
-        self._cur.execute('''SELECT * FROM sensor''')
-        sensor = self._cur.fetchall()
         for bo in board:
             bo.update({'sensor':[]})
-            light=0
-            air=0
-            elec=0
+            self._cur.execute("SELECT sid,sname FROM sensor WHERE boid = %s",(bo['boid']))
+            sensor = self._cur.fetchall()
             for s in sensor:
-                if(bo['boid']==s['boid']):
-                #SELECT COUNT(CASE WHEN tid = 1 THEN 1 END) AS light ,COUNT(CASE WHEN tid = 2 THEN 1 END) AS elec  ,COUNT(CASE WHEN tid = 3 THEN 1 END) AS air FROM YourTable
-                    bo['sensor'].append(s)
-                    if(s['tid']==1):
-                        light=light+1
-                    if(s['tid']==2):
-                        elec=elec+1
-                    if(s['tid']==3):
-                        air=air+1
-            bo.update({'type':{'light':light,'elec':elec,'air':air}})
+                bo['sensor'].append(s)
+            self._cur.execute("SELECT COUNT(CASE WHEN tid = 1 THEN 1 END) AS light ,COUNT(CASE WHEN tid = 2 THEN 1 END) AS elec  ,COUNT(CASE WHEN tid = 3 THEN 1 END) AS air FROM sensor WHERE boid = %s",(bo['boid']))
+            count = self._cur.fetchall()
+            bo.update({'type':{'light':count[0]['light'],'elec':count[0]['elec'],'air':count[0]['air']}})
                     
         return board
+        
+    def register_add(self,bomac,sensor):
+        self._cur.execute("INSERT INTO board (bomac,register) VALUES (%s, 0)",(bomac))
+        self._cur.execute("SELECT * FROM board WHERE bomac = %s",(bomac))
+        board = self._cur.fetchall()
+        for s in sensor:
+            #self._cur.execute("INSERT INTO sensor (tid,boid) VALUES (%s, %s)",(s['tid'],board['boid'])) #waiting for param valid
+            print('')
+        
+    def register_register(self,boid,sensor,rid):
+        self._cur.execute("UPDATE board SET rid=%s , register = 1 WHERE boid = %s",(rid,boid))
+        for s in sensor:
+            #self._cur.execute("UPDATE sensor SET sname=%s WHERE sid = %s",(s['sname'],s['sid'])) #waiting for param valid
+            print('')
+        
+    def register_del(self,boid):
+        self._cur.execute("DELETE FROM sensor WHERE boid = %s",(boid))   # waiting for rules to delete
+        self._cur.execute("DELETE FROM board WHERE boid = %s",(boid))
+        
+    
+    def room_add(self,rname,fid):
+        self._cur.execute("INSERT INTO room (rname,fid) VALUES (%s,%s)",(rname,fid))
+    
+    def room_edit(self,rid,rname,fid):
+        self._cur.execute("UPDATE room SET rname = %s , fid = %s WHERE rid = %s",(rname,fid,rid))
+        
+    def room_del(self,rid):
+        self._cur.execute("UPDATE board INNER JOIN room ON board.rid = room.rid SET board.register= 0 , board.rid=null WHERE room.rid = %s",(rid)) #SET board.rid to null and change board status to unregister
+        self._cur.execute("DELETE FROM room WHERE rid = %s",(rid))
+    
+    def floor_add(self,fname,bid):
+        self._cur.execute("INSERT INTO floor (fname,bid) VALUES (%s,%s)",(fname,bid))
+    
+    def floor_edit(self,fid,fname,bid):
+        self._cur.execute("UPDATE floor SET fname = %s , bid = %s WHERE fid = %s",(fname,bid,fid))
+        
+    def floor_del(self,fid):
+        self._cur.execute("UPDATE board INNER JOIN room ON board.rid = room.rid INNER JOIN floor ON room.fid=floor.fid SET board.register= 0 , board.rid=null WHERE floor.fid = %s",(fid))
+        self._cur.execute("DELETE FROM room WHERE fid = %s",(fid))
+        self._cur.execute("DELETE FROM floor WHERE fid = %s",(fid))
+        
+    def building_add(self,bname):
+        self._cur.execute("INSERT INTO building (bname) VALUES (%s)",(bname))
+    
+    def building_edit(self,bid,bname):
+        self._cur.execute("UPDATE building SET bname = %s WHERE bid = %s",(bname,bid))
+        
+    def building_del(self,bid):
+        self._cur.execute("UPDATE board INNER JOIN room ON board.rid = room.rid INNER JOIN floor ON room.fid=floor.fid INNER JOIN building ON building.bid=floor.bid SET  board.register= 0 , board.rid=null WHERE building.bid = %s",(bid))
+        self._cur.execute("DELETE room FROM room INNER JOIN floor ON room.fid = floor.fid WHERE floor.bid = %s",(bid))
+        self._cur.execute("DELETE FROM floor WHERE bid = %s",(bid))
+        self._cur.execute("DELETE FROM building WHERE bid = %s",(bid))
+        
+        
+    #board.rid can be null
+    #sensor type_tid must change to only tid
+        
     
--- Docker/web/route/demo.py
+++ Docker/web/route/demo.py
@@ -16,11 +16,11 @@ def sensor_list():
     return jsonify(query.sensor_list())
     
 @app.route('/room/list',methods=['GET'])
-def room_list():
-    return jsonify(query.room_list())
+def all_room_list():
+    return jsonify(query.all_room_list())
     
     
-@app.route('/sensor/edit',methods=['POST'])
+@app.route('/sensor/edit',methods=['UPDATE'])
 def sensor_edit():
     sid = request.form['sid']
     sname = request.form['sname']
@@ -36,3 +36,78 @@ def sensor_del():
 @app.route('/register/list',methods=['GET'])
 def register_list():
     return jsonify(query.register_list())
+    
+@app.route('/register/del',methods=['DELETE'])
+def register_del():
+    boid = request.form['boid']
+    query.register_del(boid)
+    return '200 OK DELETE'
+
+#@app.route('/register/add',methods=['POST'])
+#def register_add():
+    #bomac = request.form['bomac']
+    #sensor = request.form['sensor']
+    #query.register_add(bomac,sensor)
+    #return jsonify(query.register_add(bomac,sensor))
+    
+@app.route('/room/add',methods=['POST'])
+def room_add():
+    rname = request.form['rname']
+    fid = request.form['fid']
+    query.room_add(rname,fid)
+    return '200 OK ADD ROOM'
+    
+@app.route('/room/edit',methods=['PUT'])
+def room_edit():
+    rid = request.form['rid']
+    rname = request.form['rname']
+    fid = request.form['fid']
+    query.room_edit(rid,rname,fid)
+    return '200 OK EDIT ROOM'
+    
+@app.route('/room/del',methods=['DELETE'])
+def room_del():
+    rid = request.form['rid']
+    query.room_del(rid)
+    return '200 OK DEL ROOM'
+    
+@app.route('/floor/add',methods=['POST'])
+def floor_add():
+    fname = request.form['fname']
+    bid = request.form['bid']
+    query.floor_add(fname,bid)
+    return '200 OK ADD FLOOR'
+    
+@app.route('/floor/edit',methods=['PUT'])
+def floor_edit():
+    fid = request.form['fid']
+    fname = request.form['fname']
+    bid = request.form['bid']
+    query.floor_edit(fid,fname,bid)
+    return '200 OK EDIT FLOOR'
+    
+@app.route('/floor/del',methods=['DELETE'])
+def floor_del():
+    fid = request.form['fid']
+    query.floor_del(fid)
+    return '200 OK DEL FLOOR'
+    
+    
+@app.route('/building/add',methods=['POST'])
+def building_add():
+    bname = request.form['bname']
+    query.building_add(bname)
+    return '200 OK ADD BUILDING'
+    
+@app.route('/building/edit',methods=['PUT'])
+def building_edit():
+    bid = request.form['bid']
+    bname = request.form['bname']
+    query.building_edit(bid,bname)
+    return '200 OK EDIT BUILDING'
+    
+@app.route('/building/del',methods=['DELETE'])
+def building_del():
+    bid = request.form['bid']
+    query.building_del(bid)
+    return '200 OK DEL BUILDING'
