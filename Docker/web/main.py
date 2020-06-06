86c86f5684ae05b6c9bbf7494ed5c933093db77c
ZuGeTor <sunsunza2009@hotmail.com>
2020-06-06 17:24:30 +0700

    Add Error handle and edit some api route

--- Docker/web/main.py
+++ Docker/web/main.py
@@ -17,5 +17,21 @@ app.register_blueprint(private.app, url_prefix="/api/private")
 def index():
     return render_template("sensor_view.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(405)
+def method_not_allow(e):
+    return render_template('405.html'), 405
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
 	app.run()
--- Docker/web/route/private.py
+++ Docker/web/route/private.py
@@ -3,61 +3,56 @@ from extensions import query
 
 app = Blueprint('Private', __name__)
     
-@app.route('/sensor/list',methods=['GET'])
+@app.route('/sensor/',methods=['GET'])
 def sensor_list():
     response = jsonify(query.sensor_list())
-    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
     
-@app.route('/room/list',methods=['GET'])
+@app.route('/room/',methods=['GET'])
 def all_room_list():
     response = jsonify(query.all_room_list())
-    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
     
+@app.route('/register/',methods=['GET'])
+def register_list():
+    return jsonify(query.register_list())
     
-@app.route('/sensor/edit',methods=['PUT'])
+@app.route('/sensor',methods=['POST'])
 def sensor_edit():
     sid = request.get_json()['sid']
     sname = request.get_json()['sname']
     query.sensor_edit(sid,sname)
     response = jsonify({'Code':'200 ok'})
-    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
     
-@app.route('/sensor/del',methods=['DELETE'])
+@app.route('/sensor/del',methods=['GET'])
 def sensor_del():
-    sid = request.get_json()['sid']
+    sid = request.args.get('id')
     query.sensor_del(sid)
     response = jsonify({'Code':'200 ok'})
-    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
     
-@app.route('/register/list',methods=['GET'])
-def register_list():
-    return jsonify(query.register_list())
-    
-@app.route('/register/del',methods=['DELETE'])
+@app.route('/register/del',methods=['GET'])
 def register_del():
-    boid = request.form['boid']
+    boid = request.args.get('id')
     query.register_del(boid)
-    return '200 OK DELETE'
+    return jsonify({'Code':'200 ok'})
 
-#@app.route('/register/add',methods=['POST'])
+#@app.route('/register',methods=['POST'])
 #def register_add():
     #bomac = request.form['bomac']
     #sensor = request.form['sensor']
     #query.register_add(bomac,sensor)
     #return jsonify(query.register_add(bomac,sensor))
     
-@app.route('/room/add',methods=['POST'])
+@app.route('/room',methods=['POST'])
 def room_add():
     rname = request.form['rname']
     fid = request.form['fid']
     query.room_add(rname,fid)
     return '200 OK ADD ROOM'
     
-@app.route('/room/edit',methods=['PUT'])
+@app.route('/room/edit',methods=['POST'])
 def room_edit():
     rid = request.form['rid']
     rname = request.form['rname']
@@ -65,20 +60,20 @@ def room_edit():
     query.room_edit(rid,rname,fid)
     return '200 OK EDIT ROOM'
     
-@app.route('/room/del',methods=['DELETE'])
+@app.route('/room/del',methods=['GET'])
 def room_del():
-    rid = request.form['rid']
+    rid = request.args.get('id')
     query.room_del(rid)
     return '200 OK DEL ROOM'
     
-@app.route('/floor/add',methods=['POST'])
+@app.route('/floor/',methods=['POST'])
 def floor_add():
     fname = request.form['fname']
     bid = request.form['bid']
     query.floor_add(fname,bid)
     return '200 OK ADD FLOOR'
     
-@app.route('/floor/edit',methods=['PUT'])
+@app.route('/floor/edit',methods=['POST'])
 def floor_edit():
     fid = request.form['fid']
     fname = request.form['fname']
@@ -86,28 +81,28 @@ def floor_edit():
     query.floor_edit(fid,fname,bid)
     return '200 OK EDIT FLOOR'
     
-@app.route('/floor/del',methods=['DELETE'])
+@app.route('/floor/del',methods=['GET'])
 def floor_del():
-    fid = request.form['fid']
+    fid = request.args.get('id')
     query.floor_del(fid)
     return '200 OK DEL FLOOR'
     
     
-@app.route('/building/add',methods=['POST'])
+@app.route('/building/',methods=['POST'])
 def building_add():
     bname = request.form['bname']
     query.building_add(bname)
     return '200 OK ADD BUILDING'
     
-@app.route('/building/edit',methods=['PUT'])
+@app.route('/building/edit',methods=['POST'])
 def building_edit():
     bid = request.form['bid']
     bname = request.form['bname']
     query.building_edit(bid,bname)
     return '200 OK EDIT BUILDING'
     
-@app.route('/building/del',methods=['DELETE'])
+@app.route('/building/del',methods=['GET'])
 def building_del():
-    bid = request.form['bid']
+    bid = request.args.get('id')
     query.building_del(bid)
     return '200 OK DEL BUILDING'
