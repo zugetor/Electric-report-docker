8cfa27dabf10c1775eea677b82f090b3189acdce
ZuGeTor <sunsunza2009@hotmail.com>
2020-07-28 16:30:28 +0700

    New DB

---
+++ Docker/db/mysql/initdb/2. db_Defdata.sql
@@ -0,0 +1,4 @@
+-- Do not Delete
+INSERT INTO `building` (`bid`, `bname`, `bprefix`) VALUES ('1', 'Not available', 'N/A');
+INSERT INTO `floor` (`fid`, `fname`, `bid`) VALUES (1, 'Not available', '1');
+INSERT INTO `room` (`rid`, `rname`, `fid`) VALUES (1, 'Not available', '1')
---
+++ Docker/db/mysql/initdb/3. db_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO `type` (`tid`, `tname`, `inf_name`) VALUES 
+		(NULL, 'Light', 'light'), 
+		(NULL, 'Electricity', 'plug'), 
+		(NULL, 'Air Conditioner', 'air'), 
+		(NULL, 'Motion', "pir");
