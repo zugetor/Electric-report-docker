460d65355a0179703b85e34daa397fc2f262d4ee
Narawit Rattanasuriya <narawit@sirisoft.co.th>
2021-04-01 21:34:55 +0700

    change influxdb query to mongodb

--- Mockup Client/mockup-client.py
+++ Mockup Client/mockup-client.py
@@ -16,8 +16,8 @@ def mockupdata2(MAC):
 	return { "MAC": MAC, "status": random.randint(0, 1)}
 
 
-host = "broker.mqttdashboard.com"
-port = 1883
+host = "localhost"
+port = 1880
 client = mqtt.Client()
 client.connect(host,port)
 for _ in range(999):
@@ -43,6 +43,36 @@ for _ in range(999):
 	client.publish("/infbuu/IF/2/IF-204/pir",json.dumps(message))
 	print("Publish: {}".format(message))
 	print("-"*15)
+
+
+	message = mockupdata1(1,"AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(2,"AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(3,"AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(4,"AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(5,"AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata("AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/dm/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/2/IF-204/dm/light",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/2/IF-204/dm/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata2("AA-BB-CC-DD-EE")
+	client.publish("/infbuu/IF/2/IF-204/pir",json.dumps(message))
+	print("Publish: {}".format(message))
+	print("-"*15)
+
+
 	message = mockupdata1(1,"11-22-33-44-55")
 	client.publish("/infbuu/IF/3/IF-304/ct/light",json.dumps(message))
 	print("Publish: {}".format(message))
@@ -65,6 +95,39 @@ for _ in range(999):
 	client.publish("/infbuu/IF/3/IF-304/pir",json.dumps(message))
 	print("Publish: {}".format(message))
 	print("-"*15)
+
+
+
+	message = mockupdata1(1,"11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(2,"11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(3,"11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(4,"11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(5,"11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata("11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-302/dm/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/3/IF-302/dm/light",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/3/IF-302/dm/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata2("11-22-33-44-55")
+	client.publish("/infbuu/IF/3/IF-304/pir",json.dumps(message))
+	print("Publish: {}".format(message))
+	print("-"*15)
+
+
+
+
 	message = mockupdata1(1,"55-55-55-55-55")
 	client.publish("/infbuu/IF/6/IF-605/ct/light",json.dumps(message))
 	print("Publish: {}".format(message))
@@ -86,4 +149,31 @@ for _ in range(999):
 	message = mockupdata2("55-55-55-55-55")
 	client.publish("/infbuu/IF/6/IF-605/pir",json.dumps(message))
 	print("Publish: {}".format(message))
+
+	message = mockupdata1(1,"55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(2,"55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(3,"55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/ct/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(4,"55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata1(5,"55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/ct/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata("55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/dm/air",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/6/IF-605/dm/light",json.dumps(message))
+	print("Publish: {}".format(message))
+	client.publish("/infbuu/IF/6/IF-605/dm/plug",json.dumps(message))
+	print("Publish: {}".format(message))
+	message = mockupdata2("55-55-55-55-55")
+	client.publish("/infbuu/IF/6/IF-605/pir",json.dumps(message))
+	print("Publish: {}".format(message))
+
 	time.sleep(60)
