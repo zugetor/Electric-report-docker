e068b56ca69c07a4f9e3e6dd1c377734b1166f19
Yossaphat Karnprajam <yossaphat.k@outlook.co.th>
2021-05-29 18:00:54 +0700

    Change init sql name

--- db/mysql/initdb/3. db_type.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `type` (`tid`, `tname`, `inf_name`) VALUES 
-		(NULL, 'Light', 'light'), 
-		(NULL, 'Electricity', 'plug'), 
-		(NULL, 'Air Conditioner', 'air'), 
-		(NULL, 'Motion', "pir");
