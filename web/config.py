98a50ac087b0525614d6bd7e6dd62a703b3039fc
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2021-07-04 16:19:15 +0700

    remove unused files and library

--- web/config.py
+++ web/config.py
@@ -12,7 +12,6 @@ class Config(object):
     ENABLE_DEV = True #Enable Development Config
     LOGIN_ONLY = True #Enable Login to all page and API
     TIME_ZONE = "Asia/Bangkok" #Time zone for check room schedule
-    INF_TABLE = ["ct","dm","pir"] #Name of table in InfluxDB
     Nofify_Template = "Rule: {rname}\nDate: {date}\nDay: {dow}\nTime: {time}\nBuilding: {building}\nfloor: {floor}\nRoom: {room}\nStatus: {status}\nLight: {light}\nPlug: {plug}\nAir: {air}\nPir: {pir}\n"
     #{rname} = Rule Name
     #{date} = Current Date at notify time
@@ -31,14 +30,8 @@ class ProductionConfig(Config):
     DEBUG = False #Disable Debug
     RECAPTCHA_PUBLIC_KEY = "" #Recaptcha V2 public key from https://www.google.com/recaptcha
     RECAPTCHA_PRIVATE_KEY = "" #Recaptcha V2 private key from https://www.google.com/recaptcha
-    RULE_UPDATE = 5 #Rule update every N minutes
-    SCHEDULE_UPDATE = 60 #Schedule update every N minutes
-    SENSOR_UPDATE = 60 #Check for new sensor every N minutes
     
 class DevelopmentConfig(Config):
     DEBUG = True #Enable Debug
     RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" #Recaptcha public key for test only
     RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe" #Recaptcha public key for test only
-    RULE_UPDATE = 0.1 #Rule update every N minutes
-    SCHEDULE_UPDATE = 0.1 #Schedule update every N minutes
-    SENSOR_UPDATE = 0.1 #Check for new sensor every N minutes
